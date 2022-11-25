1ffe38ad6d77617442becffc83e86e388748dae7
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2022-11-25 13:21:03 -0300

    feat CRIANDO TELA DE LOGIN

--- src/main/java/web/mutbrocha/Application.java
+++ src/main/java/web/mutbrocha/Application.java
@@ -2,6 +2,8 @@ package web.mutbrocha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/web/mutbrocha/WebSecurityConfig.java
+++ src/main/java/web/mutbrocha/WebSecurityConfig.java
@@ -4,54 +4,34 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@EnableWebSecurity
-@ComponentScan("com")
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private DataSource dataSource;
 
-
-	@Bean
-	public DaoAuthenticationProvider getDaoAuthenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		// call the userDetailsService() method here
-		authProvider.setUserDetailsService(userDetailsService());
-		authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-		return authProvider;
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/index/**").permitAll().anyRequest().authenticated().and()
+				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/", true).permitAll())
+				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login")).csrf().disable();
 	}
-	
-	  @Bean
-	    public UserDetailsService userDetailsService() {
-	        UserDetails user = User.withDefaultPasswordEncoder()
-	                .username("user")
-	                .password("user")
-	                .roles("USER")
-	                .build();
-	        UserDetails admin = User.withDefaultPasswordEncoder()
-	                .username("admin")
-	                .password("admin")
-	                .roles("ADMIN", "USER")
-	                .build();
-	        return new InMemoryUserDetailsManager(user, admin);
-	    }
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder);
+	}
+    
 }
